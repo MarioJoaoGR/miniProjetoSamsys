4ff3ec017887ee0920bdd2811df5db3b359c5058
Mario Joao <1221019@isep.ipp.pt>
2025-03-09 15:33:53 +0000

    Delete Book BE, all implementations missing integration testing wich will be done in a postman collection when all are running #4

--- src/Domain/Books/IBookService.cs
+++ src/Domain/Books/IBookService.cs
@@ -6,6 +6,7 @@ namespace DDDNetCore.Domain.Books
     public interface IBookService
     {
         Task<BookDto> AddAsync(CreatingBookDto dto);
+        Task<BookDto> DeleteAsync(BookId id);
         Task<BookDto> GetByIdAsync(BookId id);
         Task<List<BookDto>> GetAllAsync();
     }
--- src/Infraestructure/Books/BookRepository.cs
+++ src/Infraestructure/Books/BookRepository.cs
@@ -25,5 +25,6 @@ namespace DDDNetCore.Infraestructure.Books
 
 
 
+
     }
 }
