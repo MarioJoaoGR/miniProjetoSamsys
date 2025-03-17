25543ec2bcd5b97931bf2c9a1c1118df4ef743db
Mario Joao <1221019@isep.ipp.pt>
2025-03-17 13:49:24 +0000

    final migration and workflow corection for git

---
+++ BACKEND/src/Migrations/20250316181005_FinalMigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DDDNetCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NIF_nif",
+                table: "Authors",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_NIF_nif",
+                table: "Authors",
+                column: "NIF_nif",
+                unique: true,
+                filter: "[NIF_nif] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Authors_NIF_nif",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "NIF_nif",
+                table: "Authors");
+        }
+    }
+}
